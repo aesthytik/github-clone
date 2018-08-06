7f790ff1a00b88981cdaa5ba58364194f5d84ef8
aesthytik <rawat.vipin4818@gmail.com>
2018-08-06 22:33:26 +0530

    minor fixes

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
         return (
             <div className="main-app">
                 <header className="main-header">
-                    <img src="GitHub-Mark.png" alt="logo" style={{height: '65px'}}/><h1><Link to="/">Github Clone</Link></h1>
+                    <img src="GitHub-Mark.png" alt="logo" style={{height: '65px'}}/><h1><Link to="/">GitHub Clone</Link></h1>
                 </header>
                 <main className="main-content">
                     {this.props.children}
